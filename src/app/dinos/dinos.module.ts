9c95f1da30937653c0495a4c0e295f4c5fe5d097
Johan Wendin <johan.wendin@cygni.se>
2018-01-30 13:11:59 +0100

    added dino header skeleton

--- src/app/dinos/dinos.module.ts
+++ src/app/dinos/dinos.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import DinoHeader from './header/header.component';
 import DinosComponent from 'app/dinos/dinos.component';
 import DinoService from 'app/dinos/dinos.service';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ DinosComponent ],
+  declarations: [ DinosComponent, DinoHeader ],
   exports:   [ DinosComponent ],
   providers: [DinoService]
 })
---
+++ src/app/dinos/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,  
+  selector: 'dino-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+
+export default class DinoHeader {
+
+}
